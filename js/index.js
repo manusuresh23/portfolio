db74cfe79991ddb44484524e105f3908e82f40a3
Manu Suresh <manusuresh23@gmail.com>
2017-01-08 23:26:47 -0500

    Fixing Resposive Timeline

--- js/index.js
+++ js/index.js
@@ -1,35 +1,7 @@
-(function() {
+// Freelancer Theme JavaScript
 
-    'use strict';
-
-    // define variables
-    var items = document.querySelectorAll(".timeline li");
-
-    // check if an element is in viewport
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    function callbackFunc() {
-        for (var i = 0; i < items.length; i++) {
-            if (isElementInViewport(items[i])) {
-                items[i].classList.add("in-view");
-            }
-        }
-    }
-
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-
+(function($) {
+    "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
@@ -48,7 +20,7 @@
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
-        $('.navbar-toggle:visible').click();
+            $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
@@ -57,4 +29,6 @@
             top: 100
         }
     })
-})(jQuery);
+
+  
+})(jQuery); // End of use strict
